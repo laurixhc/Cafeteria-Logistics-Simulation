e0b1a17379d1a2f6aa2a8768d17bb03d9df707b0
laurixhc <laura.henriquez.cazorla@gmail.com>
2025-09-20 11:40:15 +0200

    Update and rename modelo2.R to model2.R

--- Modelos/model2.R
+++ Modelos/model2.R
@@ -289,56 +289,55 @@ ServicioCO = function(N11, N12, N13, N2, SUMA11, SUMA12, SUMA13, SUMA2, TM, TANT
 
 #   2. Dining room
 
-#En este problema tendremos en cuenta el aforo de la cafeteria
-#Consideraremos: 1 microondas que no se estropea, 1 camarero y no tardamos 
-#tiempo en sentarnos.
+#In this problem we will take into account the capacity of the cafeteria
+#We'll consider: 1 microwave that doesn't break down, 1 waiter, and it doesn't take long to get seated.
 
-#N11 = Número de personas en la etapa 1.1
-#N12 = Número de personas en la etapa 1.2
-#N13 = Número de personas en la la etapa 1.3
-#N2 = Número de personas en la la etapa 2
+#N11 = Number of people on the stage 1.1
+#N12 = Number of people on the stage 1.2
+#N13 = Number of people on the stage 1.3
+#N2 = Number of people on the stage 2
 
-#TM = instante en el tiempo de simulación
-#TANT = instante anterior
-#Tmax = tiempo máximo de simulación
+#TM = instant in simulation time
+#TANT = previous moment
+#Tmax = maximum simulation time
 
-#DLMI = Distribución de tiempos de llegada al microondas
-#DLCA = Distribución de tiempos de llegada a la cafetería
+#DLMI = Distribution of arrival times at the microwave
+#DLCA = Distribution of arrival times at the cafeteria
 
-#DSMI = Distribución tiempos de servicio del microondas
-#DSME = Distribución tiempos de servicio del menú
-#DSBA = Distribución tiempos de servicio de la barra
-#DSCO = Distribución tiempos de servicio del comedor
+#DSMI = Distribution of microwave service times
+#DSME = Distribution of menu service times
+#DSBA = Distribution of bar service times
+#DSCO = Distribution of dining room service times
 
-#TLMI = Instante de la próxima llegada al microondas
-#TLCA = Instante de la próxima llegada a la cafetería
+#TLMI = Instant of the next arrival at the microwave
+#TLCA = Snapshot of the next arrival at the cafeteria
 
-#TSMI = Instante de la próxima finalización del servicio del microondas
-#TSME = Instante de la próxima finalización del servicio del menú
-#TSBA = Instante de la próxima finalización del servicio de la barra
-#TSCO = Instante de la próxima finalización del servicio del comedor
+#TSMI = Instant of the next microwave service completion
+#TSME = Instant of the next menu service completion
+#TSBA = Instant of the next bar service completion
+#TSCO = Instant of the next dining room service completion
 
-#Asientos = Vector de asientos que indica si está vacio el asiento y 
-#           si no el próximo fin de comida de ese asiento
-#Microondas= Vector que indica qué microondas están libres con NA u ocupados
-#            con el próximo fin de servicio
-#Camareros= Vector que indica qué camareros están libres con NA u ocupados con
-#           el próximo fin de servicio
+#Asientos = Seat vector indicating whether the seat is empty and 
+#           if not the next meal end for that seat
+#Microondas= Vector indicating which microwaves are free with NA or busy
+#            with the next end of service
+#Camareros= Vector indicating which waiters are free with NA or busy with
+#           the next end of service
 
-#SUMA11 = Contador suma acumulada etapa 1.1
-#SUMA12 = Contador suma acumulada etapa 1.2
-#SUMA13 = Contador suma acumulada etapa etapa 1.3
-#SUMA2 = Contador suma acumulada etapa etapa 2
+#SUMA11 = Counter cumulative sum stage 1.1
+#SUMA12 = Counter cumulative sum stage 1.2
+#SUMA13 = Counter cumulative sum stage 1.3
+#SUMA2 = Counter cumulative sum stage 2
 
-#total_llegadas11 = número de personas que llegan a la etapa 1.1(microondas)
-#total_llegadas12 = número de personas que llegan a la etapa 1.2(menú)
-#total_llegadas13 = número de personas que llegan a la etapa 1.3(cafetería)
-#total_llegadas = número de personas que llegan a la etapa 2(comedor)
+#total_llegadas11 = number of people who reach the stage 1.1(microwave)
+#total_llegadas12 = number of people who reach the stage 1.2(menu)
+#total_llegadas13 = number of people who reach the stage 1.3(cafeteria)
+#total_llegadas = number of people who reach the stage 2(dining room)
 
-#total_servicio11 = número de personas que han sido atendidas en el mircoondas
-#total_servicio12 = número de personas que han sido atendidas en el menú
-#total_servicio13 = número de personas que han sido atendidas en la barra
-#total_servicio2 = número de personas que se han sentado
+#total_servicio11 = number of people who have been served at the microwave
+#total_servicio12 = number of people who have been served at the menu
+#total_servicio13 = number of people who have been served at the bar
+#total_servicio2 = number of people seated
 
 ##############PARAMETERS##############
 lambdamic = 2 #people who come to the microwave per minute
@@ -512,6 +511,7 @@ print("Average number of people in the dining room")
 SUMA2/TM
 
 TM
+
 
 
 
